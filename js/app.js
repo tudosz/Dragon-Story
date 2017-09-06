df1340dee5ae7ed0a907e9c53af377dc9395b835
Przemyslaw Pajdosz <przemyslaw.pajdosz@gmail.com>
2017-09-06 18:23:16 +0200

    work in progress on character introduction

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+ 
+    var liEle = $('.characters li button');
+    var divEle = $('.charactersGallery div');
+    console.log(liEle);
+    console.log(divEle);
+
+    $(liEle).on('click', function(){
+        var thisTab = $(this).index();
+        $(divEle).hide();
+        liEle.removeClass('selected');
+        $(this).addClass('selected');
+        $(divEle[thisTab]).addClass('selected');
+        $(divEle[thisTab]).fadeIn(600);
+        console.log($(divEle[thisTab]));
+        console.log('działa');
+    });
+
+
+
+
+});
