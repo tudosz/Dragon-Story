b15f8b00ad13be8b0cd063b3c1661df4353a07bb
Przemyslaw Pajdosz <przemyslaw.pajdosz@gmail.com>
2017-09-06 19:04:52 +0200

    character introduction working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
  
-    var liEle = $('.characters li button');
+    var liEle = $('.characters li');
     var divEle = $('.charactersGallery div');
     console.log(liEle);
     console.log(divEle);
@@ -8,9 +8,9 @@ $(document).ready(function () {
     $(liEle).on('click', function(){
         var thisTab = $(this).index();
         $(divEle).hide();
-        liEle.removeClass('selected');
-        $(this).addClass('selected');
-        $(divEle[thisTab]).addClass('selected');
+        // liEle.removeClass('selected');
+        // $(this).addClass('selected');
+        // $(divEle[thisTab]).addClass('selected');
         $(divEle[thisTab]).fadeIn(600);
         console.log($(divEle[thisTab]));
         console.log('działa');
